3218e948b48fbfec0f9364f858fb5e9df1b8ebd0
n0vella <n0vella@outlook.com>
2025-10-20 20:54:48 +0200

    basic logging support

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,26 @@
+import logging
+import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
+
+
+# region: logging
+logging_format = (
+    os.getenv("LOG_FORMAT") or "%(asctime)s (%(name)s) [%(levelname)s]: %(message)s"
+)
+
+logger = logging.getLogger(__package__)
+
+logging.basicConfig(
+    filename=os.getenv("LOG_FILE"),
+    level=os.getenv("LOG_LEVEL"),
+    format=logging_format,
+    datefmt="%d/%m/%Y %H:%M:%S",
+)
+
+logging.getLogger("werkzeug").setLevel(os.getenv("LOG_LEVEL") or "WARNING")
+
+logger.info("Starting Youtube Podcast")
+# endregion
